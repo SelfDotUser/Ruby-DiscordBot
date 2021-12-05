a87f80693294798943075164b8911bf531b121ab
SelfDotUser <day-alameda.00@icloud.com>
2021-12-05 04:25:04 -0800

    fixed configs

--- src/Ruby.js
+++ src/Ruby.js
@@ -1,4 +1,4 @@
-import configs from './config.json'
+// import configs from './config.json'
 import fetch from "node-fetch";
 import { ChartJSNodeCanvas } from "chartjs-node-canvas";
 import { Client, Intents, MessageActionRow, MessageButton } from "discord.js";
@@ -228,7 +228,7 @@ client.on("interactionCreate", (interaction) => {
     }
 });
 
-client.login(configs.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 /*
 node --experimental-json-modules Ruby.js
